dbd89a9bfa7060bac7a6b49dfa26628b345929cd
Alberto <betim009@gmail.com>
2022-06-23 19:20:19 -0300

    until req 8 and lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
-import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
+import Missions from './components/Missions';
 
 class App extends React.Component {
   render() {
