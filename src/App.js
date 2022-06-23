ce3b7afb4f831d79be1eeb344a7fc704f7422e1e
Alberto <betim009@gmail.com>
2022-06-23 15:07:09 -0300

    Testando lint dos Requisitos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
 
---
+++ src/components/SolarSystem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Title from './Title';
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+class SolarSystem extends React.Component {
+  render() {
+    console.log(planets);
+    return (
+      <div>
+        <div data-testid="solar-system" />
+        <Title headline="Planetas" />
+        {planets.map((planet) => (
+          <PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+export default SolarSystem;
